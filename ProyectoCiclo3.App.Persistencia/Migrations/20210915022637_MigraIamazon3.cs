b22f5032d3b9b51d3fc39fd10d13ba7ad454733b
itsvlntn0 <laurasilva.55555@gmail.comgit config --global user.name>
2021-09-21 20:28:28 -0500

    First Commit

---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20210915022637_MigraIamazon3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class MigraIamazon3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "peso",
+                table: "Encomiendas",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "peso",
+                table: "Encomiendas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
